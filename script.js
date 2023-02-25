b0467f011230b52ca0eeac2c33fc562e70b1e501
Denis Kawata <kawatadenis@gmail.com>
2023-02-25 20:41:03 -0300

    cambios en skills

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function seleccionar(link){
     opciones[0].className = "";
     opciones[1].className = "";
     opciones[2].className = "";
-    opciones[3].className = "";
-    opciones[4].className = "";
     link.className = "seleccionado";
 
     //hacemos desaparecer el menu una vez que se ha seleccionado una opcion en el modo responsive
@@ -23,19 +21,3 @@ function responsiveMenu() {
     }
 }
 
-//detecto el scrolling para aplicar la animación de las barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades()
-};
-
-//funcion que aplica la animación de la barra de habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >=300){
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
-    }
-}
